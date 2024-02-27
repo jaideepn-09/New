dab0ab9e3cd3d4fdd96067fa2ba4f163bd593f83
Jaideep N <jaideepn3590@duck.com>
2024-02-27 23:42:41 +0530

    first commit

---
+++ untitled5.py
@@ -0,0 +1,22 @@
+list_name = ['man','women','girl','boy']
+# print("< {} --> {} >".format(list_name[-2],list_name[-3]))
+# list_name.append("child")
+# print(list_name)
+# list_name.extend(['adult','young'])
+# print(list_name)
+
+# list_ex = ['hi','hello','namste']
+# list_name.extend(list_ex)
+# print(list_name)
+
+# list_name.insert(0,'girl')
+# print(list_name)
+
+# some = list_name[:-2]
+# print(some)
+
+# hi = 'hello'[:-1]
+# print(hi)
+
+a = list_name.index('women')
+print(a)
